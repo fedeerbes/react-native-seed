5491d20ad6095ec94798c13488360772985e9b60
Esteban Erbes <fedeerbes@gmail.com>
2017-03-12 13:04:49 -0300

    seed

---
+++ seed/index.android.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class seed extends Component {
+  render() {
+    console.log('Hola mundo');
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hola Mundo!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('seed', () => seed);
